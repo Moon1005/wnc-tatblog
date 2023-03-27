08c4b23783c6cf22ea2a828e5da769d75dbc65ce
Moon1005 <tain3509@gmail.com>
2023-03-27 15:22:27 +0700

    Lab 3 homework

---
+++ src/TipsAndTricks/TatBlog.Core/DTO/PostItem.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TatBlog.Core.Contracts;
+using TatBlog.Core.Entities;
+
+namespace TatBlog.Core.DTO
+{
+    public class PostItem : IEntity
+    {
+        //Mã bài viết
+        public int Id { get; set; }
+        //Tiêu đề bài viết
+        public string Title { get; set; }
+        //Mô tả hay giới thiệu ngắn về nội dung
+        public string ShortDescription { get; set; }
+        // nội dung chi tiết của bài viết
+        public string Description { get; set; }
+        //Metadata
+        public string Meta { get; set; }
+        //Tên định danh để tạo URL
+        public string UrlSlug { get; set; }
+        //đường dẫn đến tập tin hình ảnh
+        public string ImageUrl { get; set; }
+        //số lượt xem, đọc bài viết
+        public int ViewCount { get; set; }
+        //trạng thái của bài viết
+        public bool Published { get; set; }
+        //ngày giờ đăng bài
+        public DateTime PostedDate { get; set; }
+        //ngày giờ cập nhật lần cuối
+        public DateTime? ModifiedDate { get; set; }
+        //mã chuyên mục
+        public int CategoryId { get; set; }
+        //mã tác giả của bài viết
+        public int AuthorId { get; set; }
+        // chuyên mục của bài viết
+        public Category Category { get; set; }
+        public string CategoryName { get; set; }
+        //tác giả của bài viết
+        public Author Author { get; set; }
+        //danh sách các từ khóa của bài viết
+        public IList<Tag> Tags { get; set; }
+    }
+}
--- src/TipsAndTricks/TatBlog.WebApp/Controllers/BlogController.cs
+++ src/TipsAndTricks/TatBlog.WebApp/Controllers/BlogController.cs
@@ -21,21 +21,26 @@ namespace TatBlog.WebApp.Controllers
 			[FromQuery(Name = "p")] int pageNumber = 1,
 			[FromQuery(Name = "ps")] int pageSize = 10)
 		{
-			// Tạo đối tượng chứa các điều kiện truy vấn
+			// Tao doi tuong chua cac dieu kien truy van
 			var postQuery = new PostQuery()
 			{
-				// Chỉ lấy những bài viết có trạng thái Published
+				// Chi lay nhung bai viet co trang thai Published
 				PublishedOnly = true,
-				// Tìm bài viết theo từ khóa
+
+				// Tim bai viet theo tu khoa
 				Keyword = keyword
 			};
-			// Truy vấn các bài viết theo điều kiện đã tạo
-			var postsList = await _blogRepository
-				.GetPagedPostsAsync(postQuery, pageNumber, pageSize);
-			// Lưu lại điều kiện truy vấn để hiển thị trong View 
+
+			// Truy van cac bai viet theo dieu kien da tao
+			var postList = await _blogRepository.GetPagedPostsAsync(postQuery, pageNumber, pageSize);
+
+			// Luu lai dieu kien  truy van de hien thi trong View
 			ViewBag.PostQuery = postQuery;
-			// Truyền danh sách bài viết vào View để render ra HTML
-			return View(postsList);
+
+			ViewBag.CurrentTime = DateTime.Now.ToString("HH:mm:ss");
+
+			// Truyen danh sach bai viet vao View de render ra HTML
+			return View(postList);
 		}
 
 		public IActionResult About() => View();
