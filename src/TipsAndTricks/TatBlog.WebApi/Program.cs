63fe8c9e2692b19c576835f1aabfd354c85f27bf
Moon1005 <tain3509@gmail.com>
2023-03-29 15:51:38 +0700

    Lab 4 homework Cau 3

---
+++ src/TipsAndTricks/TatBlog.WebApi/Program.cs
@@ -0,0 +1,15 @@
+using TatBlog.WebApi.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+{
+	builder.ConfigureCors()
+		.ConfigureNLog() 
+		.ConfigureServices()
+		.ConfigureSwaggerOpenApi();
+}
+
+var app = builder.Build();
+{
+	app.SetupRequestPipeline();
+	app.Run();
+}
